95df5116af1392dac24763abe49b5f70dfdc25aa
Nasadmi <samueldiego828@gmail.com>
2024-02-27 09:53:23 +0100

    Update to simple game

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const battleBg = document.querySelector("div#battle-bg");
 
 let hideElements;
 
+const winLose = document.querySelector('.win-lose-message')
+
+const playAgainBtn = document.querySelector('#continue')
+
 const movements = document.querySelectorAll(
   "main.content section.game div.movement"
 );
@@ -60,13 +64,62 @@ class Game {
     const housePickedMove = document.querySelector("main.content section.game div.movement.house-picked").getAttribute("data-movement")
     const userPickedMove = document.querySelector("main.content section.game div.movement.selected").getAttribute("data-movement")
 
+    let WinOrLose
+
     if (userPickedMove === 'scissors') {
-      
+      switch (housePickedMove) {
+        case possibles.scissors.win:
+          WinOrLose = true
+          this.boostScore()
+        break;
+
+        case possibles.scissors.lose:
+          WinOrLose = false
+          this.reduceScore()
+        break;
+      }
     }
+
+    if (userPickedMove === 'rock') {
+      switch(housePickedMove) {
+        case possibles.rock.win:
+          WinOrLose = true
+          this.boostScore()
+        break;
+
+        case possibles.rock.lose:
+          WinOrLose = false
+          this.reduceScore()
+        break;
+      }
+    }
+
+    if (userPickedMove === 'paper') {
+      switch (housePickedMove) {
+        case possibles.paper.win:
+          WinOrLose = true
+          this.boostScore()
+        break;
+
+        case possibles.paper.lose:
+          WinOrLose = false
+          this.reduceScore()
+        break;
+      }
+    }
+
+    return WinOrLose
   }
 }
 
 const game = new Game();
+
+function resetMovements() {
+  movements.forEach((move) => {
+    move.classList.remove("selected", "hide", "house-picked");
+    move.addEventListener("click", manageMovement);
+  });
+}
 
 function manageMovement(e) {
   e.stopPropagation();
@@ -88,17 +141,32 @@ function manageMovement(e) {
     hideElements = document.querySelectorAll(
       "main.content section.game div.movement.hide"
     );
-    hideElements[
-      Math.floor(Math.random() * hideElements.length)
-    ].classList.remove("hide");
-    hideElements[Math.floor(Math.random() * hideElements.length)].classList.add(
-      "house-picked"
-    );
-  }, 2500);
+
+    let element = hideElements[Math.floor(Math.random() * hideElements.length)]
+
+    element.classList.remove('hide')
+
+    element.classList.add("house-picked");
+    setTimeout(() => {
+      if (game.compareResult() === true) {
+        document.querySelector('.win-lose-message h1').innerText = 'You Win'
+      } else {
+        document.querySelector('.win-lose-message h1').innerText = 'You Lose'
+      }
+      winLose.classList.remove('hide')
+    }, 500)
+  }, 3500);
 }
 
 movements.forEach((move) => {
   move.addEventListener("click", manageMovement);
+});
+
+playAgainBtn.addEventListener("click", (e) => {
+  battleBg.classList.add("hide");
+  movementsBg.classList.remove("hide");
+  winLose.classList.add("hide");
+  resetMovements();
 });
 
 scoreSpan.innerText = `${score}`;
